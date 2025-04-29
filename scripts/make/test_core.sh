65d95d2990230352514ac06c4949afa289585d88
LincolnCB-virt <lcravenbrightman@gmail.com>
2025-04-29 12:48:34 -0400

    Added basic skeleton for testbenches as part of the make process

---
+++ scripts/make/test_core.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Build a PetaLinux project for the given board and project
+# Arguments: <vendor> <core>
+if [ $# -ne 2 ]; then
+    echo "[CORE TEST SCRIPT] ERROR:"
+    echo "Usage: $0 <vendor> <core>"
+    exit 1
+fi
+
+# Store the positional parameters in named variables and clear them
+VENDOR=${1}
+CORE=${2}
+set --
+
+###
+# Your code here to create
+# custom_cores/${VENDOR}/cores/${CORE}/tests/test_certificate
+###
